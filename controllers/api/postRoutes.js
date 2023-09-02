9a7f47e06899b686e0d8c1ace68e2b488d06f3e5
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-09-02 18:31:10 -0400

    post routes are being hard to figure out

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -4,13 +4,14 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newPost = await Post.create({
+    let newPost = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
     res.status(200).json(newPost);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
